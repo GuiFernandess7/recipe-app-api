e8cfa757864429aa5bbc9e88edb98e8f99348ff9
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-11-13 15:17:28 -0300

    Advanced filtering added

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -25,6 +25,23 @@ from core.models import (
 from core.models import Recipe
 from recipe import serializers
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'tags',
+                OpenApiTypes.STR,
+                description='Comma separated list of IDs to filter'
+            ),
+            OpenApiParameter(
+                'ingredients',
+                OpenApiTypes.STR,
+                description='Comma separated list of ingredients IDs to filter'
+            )
+        ]
+    )
+)
+
 
 class RecipeViewSets(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
@@ -40,7 +57,6 @@ class RecipeViewSets(viewsets.ModelViewSet):
 
     def get_queryset(self):
         """Retrieve recipes for only authenticated users."""
-        #return self.queryset.filter(user=self.request.user).order_by('-id')
         tags = self.request.query_params.get('tags')
         ingredients = self.request.query_params.get('ingredients')
         queryset = self.queryset
@@ -82,6 +98,17 @@ class RecipeViewSets(viewsets.ModelViewSet):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'assigned_only',
+                OpenApiTypes.INT, enum=[0, 1],
+                description='Filter by items assigned to recipes',
+            )
+        ]
+    )
+)
 
 class BaseRecipeAtrrViewSet(mixins.DestroyModelMixin,
                 mixins.UpdateModelMixin,
@@ -93,7 +120,15 @@ class BaseRecipeAtrrViewSet(mixins.DestroyModelMixin,
 
     def get_queryset(self):
         """Filter queryset to authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(
+                recipe__isnull=False
+            )
+        return queryset.filter(user=self.request.user).order_by('-name').distinct()
 
 
 class TagViewSet(BaseRecipeAtrrViewSet):
